d6f986d254e80757c1837a7e1dce9a70f1abd756
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-03-26 21:57:06 +0530

    Done upto Db Context

---
+++ DemoForecastSC/DemoForecastSC.Server/Models/LiveForecast.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoForecastSC.Server.Models
+{
+	[Table("live-forecast")]
+	public class LiveForecast
+	{
+			[Key]
+			[Column("id")]
+			public int Id { get; set; }
+
+			[Column("location")]
+			public string Location { get; set; }
+
+			[Column("date")]
+			public DateOnly Date { get; set; }
+
+			[Column("wind")]
+			public string Wind { get; set; }
+
+			[Column("air-quality")]
+			public string AirQuality { get; set; }
+
+			[Column("celsius")]
+			public string Celsius { get; set; }
+
+			[Column("fahrenheit")]
+			public string Fahrenheit { get; set; }
+
+			[Column("summary")]
+			public string Summary { get; set; }
+		
+	}
+}
