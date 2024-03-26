e5505b42e59a5eef3d7d1a6a1d340f358d5b54a7
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-03-27 00:21:40 +0530

    Added Controller

---
+++ DemoForecastSC/DemoForecastSC.Server/Controllers/LiveForecastController.cs
@@ -0,0 +1,34 @@
+﻿using DemoForecastSC.Server.DbContexts;
+using DemoForecastSC.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoForecastSC.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LiveForecastController : ControllerBase
+	{
+		private readonly  ForecastDbContext forecastDbContext;
+
+		public LiveForecastController(ForecastDbContext forecastDb)
+		{
+			forecastDbContext = forecastDb;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SaveUserInfo([FromBody] LiveForecast liveForecast)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			// Save user data to the database
+			forecastDbContext.liveForecasts.Add(liveForecast);
+			await forecastDbContext.SaveChangesAsync();
+
+			return Ok(liveForecast);
+		}
+	}
+}
